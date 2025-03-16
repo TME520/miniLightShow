f18917f4830f309d3b30be59fb05ef08bae6bfce
TME520 <tme520@gmail.com>
2025-03-16 11:59:54 +1100

    Rework main cycle (2)

--- miniLightShow.ino
+++ miniLightShow.ino
@@ -9,39 +9,34 @@ void setup() {
   strip.begin();
   Serial.begin(9600);
   randomSeed(analogRead(0));
+  strip.setBrightness(127);
 }
 
 void loop() {
-  strip.setBrightness(127);
-  
   chase(strip.Color(255, 0, 0)); // Red
   chase(strip.Color(0, 255, 0)); // Green
   chase(strip.Color(0, 0, 255)); // Blue
-  
-  chase(strip.Color(255, 0, 0)); // Red
-  chase(strip.Color(0, 0, 255)); // Blue
-  chase(strip.Color(255, 0, 0)); // Red
-  chase(strip.Color(0, 0, 255)); // Blue
-  chase(strip.Color(255, 0, 0)); // Red
-  chase(strip.Color(0, 0, 255)); // Blue
-  
+
+  breathe(strip.Color(255, 0, 0)); // Red
+  breathe(strip.Color(0, 255, 0)); // Green
+  breathe(strip.Color(0, 0, 255)); // Blue
+
+  cycle_red();
+  cycle_green();
+  cycle_blue();
+
   breathe(strip.Color(random(0, 256), random(0, 256), random(0, 256)));
   chase(strip.Color(random(0, 256), 0, 0)); // Random red
   chase(strip.Color(0, random(0, 256), 0)); // Random green
   chase(strip.Color(0, 0, random(0, 256))); // Random blue
   breathe(strip.Color(random(0, 256), random(0, 256), random(0, 256)));
   
-  breathe(strip.Color(255, 0, 0));
-  cycle_red();
-  breathe(strip.Color(255, 0, 0));
-
-  breathe(strip.Color(0, 255, 0));
-  cycle_green();
-  breathe(strip.Color(0, 255, 0));
-
-  breathe(strip.Color(0, 0, 255));
-  cycle_blue();
-  breathe(strip.Color(0, 0, 255));
+  chase(strip.Color(255, 0, 0)); // Red
+  chase(strip.Color(0, 0, 255)); // Blue
+  chase(strip.Color(255, 0, 0)); // Red
+  chase(strip.Color(0, 0, 255)); // Blue
+  chase(strip.Color(255, 0, 0)); // Red
+  chase(strip.Color(0, 0, 255)); // Blue
 
   breathe(strip.Color(255, 0, 0)); // Red
   chase(strip.Color(255, 0, 0)); // Red
@@ -83,6 +78,7 @@ void loop() {
 }
 
 static void chase(uint32_t c) {
+  strip.setBrightness(127);
   for(uint16_t i=0; i<strip.numPixels()+4; i++) {
     strip.setPixelColor(i , c); // Draw new pixel
     strip.setPixelColor(i-4, 0); // Erase pixel a few steps back
@@ -93,14 +89,6 @@ static void chase(uint32_t c) {
 
 static void breathe(uint32_t c) {
   for(uint16_t l=0; l<3; l++) {
-    for(uint16_t b=127; b>10; b--) {
-      strip.setBrightness(b);
-      for(uint16_t i=0; i<40; i++) {
-        strip.setPixelColor(i , c);
-      }
-      strip.show();
-      delay(25);
-    }
     for(uint16_t b=10; b<128; b++) {
       strip.setBrightness(b);
       for(uint16_t i=0; i<40; i++) {
@@ -109,11 +97,19 @@ static void breathe(uint32_t c) {
       strip.show();
       delay(10);
     }
+    for(uint16_t b=127; b>10; b--) {
+      strip.setBrightness(b);
+      for(uint16_t i=0; i<40; i++) {
+        strip.setPixelColor(i , c);
+      }
+      strip.show();
+      delay(25);
+    }
   }
-  strip.setBrightness(127);
 }
 
 static void cycle() {
+  strip.setBrightness(127);
   for(uint16_t r=0; r<256; r+=32) {
     // RED
     for(uint16_t g=0; g<256; g+=32) {
@@ -132,6 +128,7 @@ static void cycle() {
 }
 
 static void cycle_red() {
+  strip.setBrightness(127);
   for(uint16_t r=0; r<256; r+=32) {
     for(uint16_t i=0; i<40; i++) {
       strip.setPixelColor(i, strip.Color(r, 0, 0));
@@ -142,6 +139,7 @@ static void cycle_red() {
 }
 
 static void cycle_green() {
+  strip.setBrightness(127);
   for(uint16_t g=0; g<256; g+=32) {
     for(uint16_t i=0; i<40; i++) {
       strip.setPixelColor(i, strip.Color(0, g, 0));
@@ -152,6 +150,7 @@ static void cycle_green() {
 }
 
 static void cycle_blue() {
+  strip.setBrightness(127);
   for(uint16_t b=0; b<256; b+=32) {
     for(uint16_t i=0; i<40; i++) {
       strip.setPixelColor(i, strip.Color(0, 0, b));
